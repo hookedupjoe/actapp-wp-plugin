847d94a081556b3b40256bdcda27a618547fa3cc
Joseph Francis <hookedupjoe@gmail.com>
2021-05-22 09:13:25 -0400

    Moved Dynamic to own class and dir - mid updates

--- actapp/widgets/ActAppWidgetManager.php
+++ actapp/widgets/ActAppWidgetManager.php
@@ -45,49 +45,49 @@ class ActAppWidgetManager {
 	public static function init() {
 		add_filter( 'block_categories',  array('ActAppWidgetManager','actapp_block_category'), 10, 2);
 		add_action('enqueue_block_editor_assets',  array('ActAppWidgetManager','actapp_init_blocks'),10,2);
-		self::gutenberg_examples_dynamic();
+		//self::gutenberg_examples_dynamic();
 	}
 
 	
-	public static function gutenberg_examples_dynamic_render_callback( $block_attributes, $content ) {
-		$tmpCount = sizeof($block_attributes);
-		if( $tmpCount == 0){
-			return 'no attributes';
-			}
-			if( $block_attributes['debug'] ){
-			return 'debug ' . $block_attributes['debug'];
-		}
+	// public static function gutenberg_examples_dynamic_render_callback( $block_attributes, $content ) {
+	// 	$tmpCount = sizeof($block_attributes);
+	// 	if( $tmpCount == 0){
+	// 		return 'no attributes';
+	// 		}
+	// 		if( $block_attributes['debug'] ){
+	// 		return 'debug ' . $block_attributes['debug'];
+	// 	}
 
-		if( $block_attributes['message'] ){
-			return '<div class="ui card"><b>message</b>: ' . $block_attributes['message'] . "</div>";
-		}
-		return 'unknown params';
-	}
+	// 	if( $block_attributes['message'] ){
+	// 		return '<div class="ui card"><b>message</b>: ' . $block_attributes['message'] . "</div>";
+	// 	}
+	// 	return 'unknown params';
+	// }
 
 
-	function gutenberg_examples_dynamic() {
+	// function gutenberg_examples_dynamic() {
 	 
-		wp_register_script(
-			'gutenberg-examples-dynamic',
-			plugins_url( 'build/block.js', ACTAPP_WIDGETS_DIR ),
-			array('wp-blocks', 'wp-element', 'wp-server-side-render', 'wp-i18n', 'wp-polyfill')
-		);
+	// 	wp_register_script(
+	// 		'gutenberg-examples-dynamic',
+	// 		plugins_url( 'build/block.js', ACTAPP_WIDGETS_DIR ),
+	// 		array('wp-blocks', 'wp-element', 'wp-server-side-render', 'wp-i18n', 'wp-polyfill')
+	// 	);
 	 
-		register_block_type( 'gutenberg-examples/example-dynamic', array(
-			'api_version' => 2,
-			'attributes' => array(
-				'message' => array(
-					'type' => 'string'
-				),
-				'debug' => array(
-					'type' => 'string'
-				),
-				),
-			'editor_script' => 'gutenberg-examples-dynamic',
-			'render_callback' => array('ActAppWidgetManager','gutenberg_examples_dynamic_render_callback'),
-		) );
+	// 	register_block_type( 'gutenberg-examples/example-dynamic', array(
+	// 		'api_version' => 2,
+	// 		'attributes' => array(
+	// 			'message' => array(
+	// 				'type' => 'string'
+	// 			),
+	// 			'debug' => array(
+	// 				'type' => 'string'
+	// 			),
+	// 			),
+	// 		'editor_script' => 'gutenberg-examples-dynamic',
+	// 		'render_callback' => array('ActAppWidgetManager','gutenberg_examples_dynamic_render_callback'),
+	// 	) );
 	 
-	}
+	// }
 	
 
 	
@@ -98,9 +98,9 @@ class ActAppWidgetManager {
 		return ACTAPP_WIDGETS_URL;
 	}
 	
-	
 }
 
+require_once ACTAPP_WIDGETS_DIR . '/blocks/ActAppDynamicCard/Object.php';
 
 add_action( 'init', array( 'ActAppWidgetManager', 'init' ) );
 
