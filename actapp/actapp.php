daa86744a2edcac7e0fcb43701731ed6e6f442b0
Joseph Francis <hookedupjoe@gmail.com>
2021-05-22 08:58:03 -0400

    blocks temp work cleanup 0522210857

--- actapp/actapp.php
+++ actapp/actapp.php
@@ -64,101 +64,19 @@ if ( !defined( 'ACTAPP_WIDGETS_URL' ) ) {
 }
 
 
-
 require_once ACTAPP_CORE_LIB . '/constants.php';
 require_once ACTAPP_CORE_LIB . '/wp-init.php';
 require_once ACTAPP_SLEEKDB_LIB . '/SleekDB.php';
 
-// //ToDo: Move this
-// add_action( 'wp_enqueue_scripts', 'actapp_remove_jquery', 100 );
-// function actapp_remove_jquery()
-// {
-//     // wp_dequeue_script( 'jquery-core-js' );
-//     // wp_deregister_script( 'jquery-core-js' );
-
-//     // wp_dequeue_script( 'jquery-core-js' );
-//     // wp_deregister_script( 'jjquery-migrate-js' );
-//     // Now the parent script is completely removed
-// }
-
-function actapp_block_category( $categories, $post ) {
-	return array_merge(
-		array(
-			array(
-				'slug' => 'actappui',
-				'title' => __( 'UI Widgets'),
-			),
-		),
-		$categories,
-	);
-}
-
-function actapp_init_blocks($theHook) {
-	
-		actapp_load_scripts($theHook);
-
-		  wp_enqueue_script(
-			'my-new-block2', 
-			ACTAPP_WIDGETS_URL . '/blocks/core-blocks.js',
-			array('wp-blocks','wp-editor','wp-element','wp-rich-text','wp-data','wp-server-side-render'),
-			true
-		  );
-
-}
-
-add_filter( 'block_categories', 'actapp_block_category', 10, 2);
-add_action('enqueue_block_editor_assets', 'actapp_init_blocks',10,2);
-
-
-
-
-
-
 
 
 
  
-function gutenberg_examples_dynamic_render_callback( $block_attributes, $content ) {
-   	$tmpCount = sizeof($block_attributes);
-   	if( $tmpCount == 0){
-	   return 'no attributes';
-   	}
-   	if( $block_attributes['debug'] ){
-		return 'debug ' . $block_attributes['debug'];
-	}
 
-   if( $block_attributes['message'] ){
-	   return '<div class="ui card"><b>message</b>: ' . $block_attributes['message'] . "</div>";
-   }
-   return 'unknown params';
-}
+require_once ACTAPP_WIDGETS_DIR . '/ActAppWidgetManager.php';
 
 
 
 
-function gutenberg_examples_dynamic() {
-    // automatically load dependencies and version
- 
-    wp_register_script(
-        'gutenberg-examples-dynamic',
-        plugins_url( 'build/block.js', __FILE__ ),
-        array('wp-blocks', 'wp-element', 'wp-server-side-render', 'wp-i18n', 'wp-polyfill')
-    );
- 
-    register_block_type( 'gutenberg-examples/example-dynamic', array(
-        'api_version' => 2,
-		'attributes' => array(
-			'message' => array(
-				'type' => 'string'
-			),
-			'debug' => array(
-				'type' => 'string'
-			),
-			),
-        'editor_script' => 'gutenberg-examples-dynamic',
-        'render_callback' => 'gutenberg_examples_dynamic_render_callback',
-    ) );
- 
-}
-add_action( 'init', 'gutenberg_examples_dynamic' );
+
 
--- actapp/widgets/ActAppWidgetManager.php
+++ actapp/widgets/ActAppWidgetManager.php
@@ -5,6 +5,9 @@ Common access points for ActApp related widgets
 
 /* package: actapp */
 
+
+
+
 class ActAppWidgetManager {
 	private static $instance;
 	public static function get_instance() {
@@ -14,25 +17,79 @@ class ActAppWidgetManager {
 		return self::$instance;
 	}
 
-	public static function init() {
-		//self::do_something_on_startup();
-		self::sidebar_plugin_register();
 
+	public static function actapp_block_category( $categories, $post ) {
+		return array_merge(
+			array(
+				array(
+					'slug' => 'actappui',
+					'title' => __( 'UI Widgets'),
+				),
+			),
+			$categories,
+		);
+	}
+
+	public static function actapp_init_blocks($theHook) {
 		
+			actapp_load_scripts($theHook);
+
+			wp_enqueue_script(
+				'actapp-core-blocks', 
+				ACTAPP_WIDGETS_URL . '/blocks/core-blocks.js',
+				array('wp-blocks','wp-editor','wp-element','wp-rich-text','wp-data','wp-server-side-render'),
+				true
+			);
+	}
+
+	public static function init() {
+		add_filter( 'block_categories',  array('ActAppWidgetManager','actapp_block_category'), 10, 2);
+		add_action('enqueue_block_editor_assets',  array('ActAppWidgetManager','actapp_init_blocks'),10,2);
+		self::gutenberg_examples_dynamic();
 	}
 
 	
+	public static function gutenberg_examples_dynamic_render_callback( $block_attributes, $content ) {
+		$tmpCount = sizeof($block_attributes);
+		if( $tmpCount == 0){
+			return 'no attributes';
+			}
+			if( $block_attributes['debug'] ){
+			return 'debug ' . $block_attributes['debug'];
+		}
 
-	public static function sidebar_plugin_register() {
+		if( $block_attributes['message'] ){
+			return '<div class="ui card"><b>message</b>: ' . $block_attributes['message'] . "</div>";
+		}
+		return 'unknown params';
+	}
+
+
+	function gutenberg_examples_dynamic() {
+	 
 		wp_register_script(
-			'plugin-sidebar-js',
-			plugins_url( 'plugin-sidebar.js', __FILE__ ),
-			array( 'wp-plugins', 'wp-edit-post', 'wp-element' )
+			'gutenberg-examples-dynamic',
+			plugins_url( 'build/block.js', ACTAPP_WIDGETS_DIR ),
+			array('wp-blocks', 'wp-element', 'wp-server-side-render', 'wp-i18n', 'wp-polyfill')
 		);
+	 
+		register_block_type( 'gutenberg-examples/example-dynamic', array(
+			'api_version' => 2,
+			'attributes' => array(
+				'message' => array(
+					'type' => 'string'
+				),
+				'debug' => array(
+					'type' => 'string'
+				),
+				),
+			'editor_script' => 'gutenberg-examples-dynamic',
+			'render_callback' => array('ActAppWidgetManager','gutenberg_examples_dynamic_render_callback'),
+		) );
+	 
 	}
-	public static function sidebar_plugin_script_enqueue() {
-		wp_enqueue_script( 'plugin-sidebar-js' );
-	}
+	
+
 	
 	public static function baseDir() {
 		return ACTAPP_WIDGETS_DIR;
@@ -41,8 +98,9 @@ class ActAppWidgetManager {
 		return ACTAPP_WIDGETS_URL;
 	}
 	
+	
 }
 
-add_action( 'enqueue_block_editor_assets', array('ActAppWidgetManager','sidebar_plugin_script_enqueue' );
+
 add_action( 'init', array( 'ActAppWidgetManager', 'init' ) );
 
