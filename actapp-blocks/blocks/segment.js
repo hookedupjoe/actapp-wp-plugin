f9804db1e0acdf8a238826a8c7607277d43655d1
Joseph Francis <hookedupjoe@gmail.com>
2021-06-05 15:51:36 -0400

    Reformat

--- actapp-blocks/blocks/segment.js
+++ actapp-blocks/blocks/segment.js
@@ -18,9 +18,9 @@
  * @since actapp 1.0.22
  */
 
- 
 
-( function ( wp, ActionAppCore ) {
+
+(function (wp, ActionAppCore) {
     var gSelectedAddItem = '';
     var gToolbarSelection = '';
 
@@ -28,149 +28,146 @@
     var useBlockProps = wp.blockEditor.useBlockProps;
     var BlockEditor = ActionAppCore.common.blocks.Editor;
     var be = wp.data.dispatch('core/editor');
-    
+
     var info = {
         name: 'segment',
         title: 'UI Segment',
         example: {
-            attributes: {color: 'blue'}
+            attributes: { color: 'blue' }
         },
         category: 'actappui',
         atts: {}
     };
     const iconEl = BlockEditor.getControlIcon(info.name);
 
-    BlockEditor.addBooleanAtts(info.atts,['raised','stacked','vertical','clearing','hasdropindicator']);
-    BlockEditor.addStringAtts(info.atts,['color','size','attached','alignment','basic', 'spotname','spotsourcetype','spotsourcename','spotsourcepartname']);
+    BlockEditor.addBooleanAtts(info.atts, ['raised', 'stacked', 'vertical', 'clearing', 'hasdropindicator']);
+    BlockEditor.addStringAtts(info.atts, ['color', 'size', 'attached', 'alignment', 'basic', 'spotname', 'spotsourcetype', 'spotsourcename', 'spotsourcepartname']);
 
     var tmpClassSpecs = {
-        boolean: ['raised','stacked','vertical','basic','clearing'],
-        string: ['color','size','attached','alignment']
-    }    
-    function getClass(theProps, theIsEditMode){
-        return BlockEditor.getStandardClass( 'ui segment', tmpClassSpecs, theProps, theIsEditMode);
+        boolean: ['raised', 'stacked', 'vertical', 'basic', 'clearing'],
+        string: ['color', 'size', 'attached', 'alignment']
     }
-    function getDisplayValue(theProps,theIsEditMode){
+    function getClass(theProps, theIsEditMode) {
+        return BlockEditor.getStandardClass('ui segment', tmpClassSpecs, theProps, theIsEditMode);
+    }
+    function getDisplayValue(theProps, theIsEditMode) {
         var props = theProps;
         var tmpClass = getClass(props, true);
-        if( theIsEditMode  ){ //&& !props.isSelected 
+        if (theIsEditMode) { //&& !props.isSelected 
             tmpClass += ' actapp-block-box';
         }
-        var tmpAtts = {className:tmpClass};
+        var tmpAtts = { className: tmpClass };
         var tmpPropAtts = theProps.attributes;
-        
+
         var tmpEls = [];
-        if( tmpPropAtts.spotname  && tmpPropAtts.spotname != ''){
-            var tmpSpotTopAtts = {spot:tmpPropAtts.spotname};
-            if( tmpPropAtts.spotsourcetype ){
+        if (tmpPropAtts.spotname && tmpPropAtts.spotname != '') {
+            var tmpSpotTopAtts = { spot: tmpPropAtts.spotname };
+            if (tmpPropAtts.spotsourcetype) {
                 tmpSpotTopAtts.sourcetype = tmpPropAtts.spotsourcetype;
                 tmpSpotTopAtts.appuse = 'blockmarkup';
             }
-            if( tmpPropAtts.spotsourcename ){
-                tmpSpotTopAtts.sourcename = tmpPropAtts.spotsourcename;                
+            if (tmpPropAtts.spotsourcename) {
+                tmpSpotTopAtts.sourcename = tmpPropAtts.spotsourcename;
             }
-            if( tmpPropAtts.spotsourcepartname ){
-                tmpSpotTopAtts.sourcepartname = tmpPropAtts.spotsourcepartname;                
+            if (tmpPropAtts.spotsourcepartname) {
+                tmpSpotTopAtts.sourcepartname = tmpPropAtts.spotsourcepartname;
             }
-            
-            tmpEls.push(el('div',tmpSpotTopAtts,''));
+
+            tmpEls.push(el('div', tmpSpotTopAtts, ''));
         }
 
-        
-        if( theIsEditMode ){
-            var tmpMe = wp.data.select( 'core/block-editor' ).getBlock(props.clientId);
-            window.tmpMe = tmpMe;
-            var tmpChildren = tmpMe.innerBlocks;
-            if(!(tmpChildren && tmpChildren.length )){
-                //--- This assures the drag and drop feature allows a drop in a new unselected segment
-                var tmpToAddElement = BlockEditor.getCommonBlock('dropindicator'); 
-                tmpPropAtts.hasdropindicator = true;
-                be.insertBlocks(tmpToAddElement,0,props.clientId);
-            } else {
-                //Find and remove drop indicator
-                if( tmpPropAtts.hasdropindicator && tmpChildren.length > 1 ){
-                    var tmpPos = -1;
-                    for( var iPos = 0 ; iPos < tmpChildren.length ; iPos++){
-                        var tmpChild = tmpChildren[iPos];
-                        if( tmpChild.name == 'actappui/dropindicator'){
-                            tmpPos = iPos;                            
-                            break;
+        if (theIsEditMode) {
+            var tmpMe = wp.data.select('core/block-editor').getBlock(props.clientId);
+            //--- If doing a preview, there is no active block in the editor
+            if (tmpMe) {
+                var tmpChildren = tmpMe.innerBlocks;
+                if (!(tmpChildren && tmpChildren.length)) {
+                    //--- This assures the drag and drop feature allows a drop in a new unselected segment
+                    var tmpToAddElement = BlockEditor.getCommonBlock('dropindicator');
+                    tmpPropAtts.hasdropindicator = true;
+                    be.insertBlocks(tmpToAddElement, 0, props.clientId);
+                } else {
+                    //Find and remove drop indicator
+                    if (tmpPropAtts.hasdropindicator && tmpChildren.length > 1) {
+                        var tmpPos = -1;
+                        for (var iPos = 0; iPos < tmpChildren.length; iPos++) {
+                            var tmpChild = tmpChildren[iPos];
+                            if (tmpChild.name == 'actappui/dropindicator') {
+                                tmpPos = iPos;
+                                break;
+                            }
+                        }
+                        if (tmpPos > -1) {
+                            tmpMe.innerBlocks.splice(tmpPos, 1);
+                            tmpPropAtts.hasdropindicator = false;
+                            ActAppBlockEditor.refreshBlockEditor();
                         }
                     }
-                    if( tmpPos > -1 ){
-                        tmpMe.innerBlocks.splice(tmpPos,1);
-                        tmpPropAtts.hasdropindicator = false;
-                        ActAppBlockEditor.refreshBlockEditor();
-                    }
-
                 }
-                
-
             }
-            tmpEls.push(el( wp.blockEditor.InnerBlocks ));             
+            tmpEls.push(el(wp.blockEditor.InnerBlocks));
         } else {
-            tmpEls.push(el( wp.blockEditor.InnerBlocks.Content ));
+            tmpEls.push(el(wp.blockEditor.InnerBlocks.Content));
         }
 
-        
         return el('div', tmpAtts, tmpEls);
-        
+
     }
 
-    wp.blocks.registerBlockType( info.category + '/' + info.name, {
+    wp.blocks.registerBlockType(info.category + '/' + info.name, {
         title: info.title,
         icon: iconEl,
         category: info.category,
         example: info.example,
         attributes: info.atts,
-        edit: function ( props ) {
-            
+        edit: function (props) {
+
             var tmpPropAtts = props.attributes;
-            if( tmpPropAtts.spotname ){
+            if (tmpPropAtts.spotname) {
 
             }
             var tmpStandardProperties = [
-                BlockEditor.getStandardProperty(props,'color', 'Segment Color', 'color' ),
-                BlockEditor.getStandardProperty(props,'size', 'Size', 'size' ),
-                BlockEditor.getStandardProperty(props,'basic', 'No Border', 'checkbox' ),
-                BlockEditor.getStandardProperty(props,'attached', 'Attached', 'attached' ),
-                BlockEditor.getStandardProperty(props,'raised', 'Raised', 'checkbox' ),
-                BlockEditor.getStandardProperty(props,'stacked', 'Stacked', 'checkbox' ),
-                BlockEditor.getStandardProperty(props,'vertical', 'Vertical', 'checkbox' ),
-                BlockEditor.getStandardProperty(props,'clearing', 'Contain Floaters', 'checkbox' )
+                BlockEditor.getStandardProperty(props, 'color', 'Segment Color', 'color'),
+                BlockEditor.getStandardProperty(props, 'size', 'Size', 'size'),
+                BlockEditor.getStandardProperty(props, 'basic', 'No Border', 'checkbox'),
+                BlockEditor.getStandardProperty(props, 'attached', 'Attached', 'attached'),
+                BlockEditor.getStandardProperty(props, 'raised', 'Raised', 'checkbox'),
+                BlockEditor.getStandardProperty(props, 'stacked', 'Stacked', 'checkbox'),
+                BlockEditor.getStandardProperty(props, 'vertical', 'Vertical', 'checkbox'),
+                BlockEditor.getStandardProperty(props, 'clearing', 'Contain Floaters', 'checkbox')
             ];
             var tmpFormatProperties = [
                 //(tmpAtts.alignment) ? '' : BlockEditor.getStandardProperty(props,'float', 'Float', 'floatleftright' ),
-                BlockEditor.getStandardProperty(props,'alignment', 'Alignment', 'alignment' )
+                BlockEditor.getStandardProperty(props, 'alignment', 'Alignment', 'alignment')
             ];
 
             var tmpDevProperties = [
-                BlockEditor.getStandardProperty(props,'spotname', 'Spot Name', 'text' ),
-                !(tmpPropAtts.spotname !='') ? '' : BlockEditor.getStandardProperty(props,'spotsourcetype', 'Spot Source Type', 'text' ),
-                !(tmpPropAtts.spotname !='') ? '' : BlockEditor.getStandardProperty(props,'spotsourcename', 'Spot Source Name', 'text' ),
-                !(tmpPropAtts.spotname !='') ? '' : BlockEditor.getStandardProperty(props,'spotsourcepartname', 'Part Name (Optional)', 'text' ),
-                !(tmpPropAtts.spotsourcename !='') ? '' : el('div',{className: 'ui button circular blue fluid', action:'updatePreview'},'Refresh Preview')
+                BlockEditor.getStandardProperty(props, 'spotname', 'Spot Name', 'text'),
+                !(tmpPropAtts.spotname != '') ? '' : BlockEditor.getStandardProperty(props, 'spotsourcetype', 'Spot Source Type', 'text'),
+                !(tmpPropAtts.spotname != '') ? '' : BlockEditor.getStandardProperty(props, 'spotsourcename', 'Spot Source Name', 'text'),
+                !(tmpPropAtts.spotname != '') ? '' : BlockEditor.getStandardProperty(props, 'spotsourcepartname', 'Part Name (Optional)', 'text'),
+                !(tmpPropAtts.spotsourcename != '') ? '' : el('div', { className: 'ui button circular blue fluid', action: 'updatePreview' }, 'Refresh Preview')
             ];
 
             var tmpSidebarPanels = [
                 BlockEditor.getSidebarPanel('Segment Options', tmpStandardProperties),
-                BlockEditor.getSidebarPanel('Formatting Options', tmpFormatProperties),                
+                BlockEditor.getSidebarPanel('Formatting Options', tmpFormatProperties),
                 BlockEditor.getSidebarPanel('Developer Options', tmpDevProperties)
             ];
 
             var tmpSidebarControls = BlockEditor.getSidebarControls(tmpSidebarPanels);
 
-            var tmpDisplayObject = getDisplayValue(props,true);
-           
-            var tmpOnAddSelect = function(theEvent){
-                var tmpThis = wp.data.select( 'core/block-editor' ).getSelectedBlock();
+            var tmpDisplayObject = getDisplayValue(props, true);
+
+            var tmpOnAddSelect = function (theEvent) {
+                var tmpThis = wp.data.select('core/block-editor').getSelectedBlock();
                 var tmpPos = 0;
-                if( tmpThis.innerBlocks && tmpThis.innerBlocks.length ){
+                if (tmpThis.innerBlocks && tmpThis.innerBlocks.length) {
                     tmpPos = tmpThis.innerBlocks.length;
                 }
                 var tmpItemToAdd = theEvent.nativeEvent.target.value || '';
                 var tmpToAddElement = BlockEditor.getCommonBlock(tmpItemToAdd);
-                wp.data.dispatch('core/editor').insertBlocks(tmpToAddElement,tmpPos,tmpThis.clientId) 
+                wp.data.dispatch('core/editor').insertBlocks(tmpToAddElement, tmpPos, tmpThis.clientId)
             }
             var tmpToolbarAdds = el(
                 wp.blockEditor.BlockControls,
@@ -180,34 +177,34 @@
                 ]
             );
 
-            gToolbarSelection =  BlockEditor.getCommonBlocksListControl(gSelectedAddItem,tmpOnAddSelect);
+            gToolbarSelection = BlockEditor.getCommonBlocksListControl(gSelectedAddItem, tmpOnAddSelect);
 
             var tmpBtnBar = '';
-            if( props.isSelected ){
+            if (props.isSelected) {
                 var tmpBarContent = [];
                 //tmpBarContent.push(el('div',{className:'ui fluid label blue mar5'},'UI Segment')),
-                tmpBarContent.push(el('div',{className:'ui compact button blue basic ',action:'beAddElement', elementname: 'header'}, 'Header'));
-                tmpBarContent.push(el('div',{className:'ui compact button blue basic ',action:'beAddElement', elementname:'message'}, 'Message'));
-                tmpBarContent.push(el('div',{className:'ui compact button blue basic ',action:'beAddElement', elementname:'image'}, 'Image'));
-                tmpBarContent.push(el('div',{className:'ui compact button blue basic ',action:'beAddElement', elementname:'cards'}, 'Cards'));
-                tmpBtnBar = el('div',{},[el('div',{className:'ui fluid label blue mar5'},'UI Segment'),el('div',{className:'ui segment raised slim'},tmpBarContent,el('div',{className:'endfloat'}))]);
+                tmpBarContent.push(el('div', { className: 'ui compact button blue basic ', action: 'beAddElement', elementname: 'header' }, 'Header'));
+                tmpBarContent.push(el('div', { className: 'ui compact button blue basic ', action: 'beAddElement', elementname: 'message' }, 'Message'));
+                tmpBarContent.push(el('div', { className: 'ui compact button blue basic ', action: 'beAddElement', elementname: 'image' }, 'Image'));
+                tmpBarContent.push(el('div', { className: 'ui compact button blue basic ', action: 'beAddElement', elementname: 'cards' }, 'Cards'));
+                tmpBtnBar = el('div', {}, [el('div', { className: 'ui fluid label blue mar5' }, 'UI Segment'), el('div', { className: 'ui segment raised slim' }, tmpBarContent, el('div', { className: 'endfloat' }))]);
             }
-            
+
             return el(
                 'div',
                 useBlockProps(),
                 [
                     tmpBtnBar,
-                    tmpSidebarControls,               
+                    tmpSidebarControls,
                     tmpDisplayObject
                 ]
             );
         },
- 
-        save: function ( props ) {
-            return getDisplayValue(props,false);
+
+        save: function (props) {
+            return getDisplayValue(props, false);
         },
-    } );
-} )( window.wp, window.ActionAppCore );
+    });
+})(window.wp, window.ActionAppCore);
 
 
